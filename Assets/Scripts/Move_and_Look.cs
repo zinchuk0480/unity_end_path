2e32be28357c6b6cb2d7dea3d47e802bd0136075
azinatullin <azinatullin@contractors.sk.ru>
2023-12-16 02:37:26 +0300

    add moving and look script

--- Assets/Scripts/Move_and_Look.cs
+++ Assets/Scripts/Move_and_Look.cs
@@ -1,19 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Move_and_Look : MonoBehaviour
 {
+
+    private Rigidbody player;
+
     public float speed = 5.0f;
     public float mouseSens = 2.0f;
+    public float jumpForce = 5.0f;
+
+    public bool onGround = true;
 
     public float verticalRotation = 0;
     public float horizontalRotation = 0;
 
+    public LayerMask cave;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GetComponent<Rigidbody>();
+        cave = 1 << 6;
     }
 
     // Update is called once per frame
@@ -21,6 +31,7 @@ public class Move_and_Look : MonoBehaviour
     {
         Look();
         Move();
+        CreateRay();
     }
 
     void Look()
@@ -39,5 +50,31 @@ public class Move_and_Look : MonoBehaviour
     {
         float input_forward = Input.GetAxis("Vertical") * speed * Time.deltaTime;
         float input_horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        transform.Translate(new Vector3(input_horizontal, 0, input_forward));
+
+        if (Input.GetKeyDown(KeyCode.Space) && onGround)
+        {
+            player.AddForce(transform.up * jumpForce * Time.deltaTime, ForceMode.Impulse);
+            onGround = false;
+        }
+    }
+
+    void CreateRay()
+    {
+        // центр экрана
+        Vector3 screenCenter = new Vector3(Screen.width / 2f, Screen.height / 2f, 0);
+
+
+        Ray ray = Camera.main.ScreenPointToRay(screenCenter);
+        Debug.DrawRay(ray.origin, ray.direction * 100f, Color.yellow);
+
+        RaycastHit hit;
+        int layer_mask = LayerMask.GetMask("used", "used_2");
+
+        if (Physics.Raycast(ray, out hit, 50, ~cave))
+        {
+            Debug.Log(LayerMask.LayerToName(hit.collider.gameObject.layer));
+        }
     }
 }
